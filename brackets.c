adfb56be7d49e4e151100ed7ba056aace686c8b4
Ilan Smith <lunnys@gmail.com>
2015-03-25 01:24:55 +0200

    brackets: new entries created at the end of list rather than begining.
    This enables reducing the number of cases that need special handling.

--- brackets.c
+++ brackets.c
@@ -2,79 +2,67 @@
 #include <stdio.h>
 
 struct bracket {
-	struct bracket *prev;
 	struct bracket *next;
-	char open;
+	struct bracket **prev;
+	char brc;
 };
 
-static int blk_open(char sym)
-{
-	return sym == '(' || sym == '[' || sym == '{';
-}
-
-static char match_close(char sym)
-{
-	return sym == ')' ? '(' : sym == ']' ? '[' : sym == '}' ? '{' : 0;
-}
+#define BRC_OPEN(sym) ((sym) == '(' || (sym) == '[' || (sym) == '{')
+#define MATCH_CLOSE(sym) ((sym) == ')' ? '(' : (sym) == ']' ? \
+	'[' : (sym) == '}' ? '{' : 0)
 
 static int check(char *text)
 {
-	struct bracket *list = NULL, *cur;
+	struct bracket *list = NULL, **cur = &list;
 	int ret = -1;
 
-	for (cur = list; *text; text++) {
-		char open;
+	for ( ; *text; text++) {
+		char brc;
 
-		if (blk_open(*text)) {
-			if (cur == list) {
-				if (!(cur = (struct bracket *)
+		if (BRC_OPEN(*text)) {
+			if (!*cur && !(*cur = (struct bracket *)
 					calloc(1, sizeof(struct bracket)))) {
-					goto Exit;
-				}
-				cur->next = list;
-				if (list)
-					list->prev = cur;
-				list = cur;
-			} else {
-				cur = cur->prev;
-			}
-			cur->open = *text;
-		} else if ((open = match_close(*text))) {
-			if (!cur || open != ((struct bracket *)cur)->open)
 				goto Exit;
-			cur = cur->next;
+			}
+			(*cur)->brc = *text;
+			(*cur)->prev = cur;
+			cur = &(*cur)->next;
+		} else if ((brc = MATCH_CLOSE(*text))) {
+			if (cur == &list || brc != ((struct bracket *)cur)->brc)
+				goto Exit;
+			cur = ((struct bracket *)cur)->prev;
 		}
 	}
-
-	ret = 0;
-
+	ret = cur == &list ? 0 : -1;
 Exit:
-	while (list) {
-		cur = list;
-		list = list->next;
-		free(cur);
+	for (cur = &list; *cur; list = *cur) {
+		cur = &(*cur)->next;
+		free(list);
 	}
-
 	return ret;
 }
 
 int main(int argc, char **argv)
 {
-	char *strings[] = {
-		"a(b)",
-		"[{}]",
-		"[(]",
-		"}{",
-		"z([{}-()]{a})",
-		"",
-		NULL
+	struct {
+		char *str;
+		int val;
+	} *t, tests[] = {
+		{ "a(b)", 0 },
+		{ "[{}]", 0 },
+		{ "[(]", -1 },
+		{ "}{", -1 },
+		{ "z([{}-()]{a})", 0 },
+		{ "[{}a ] b", 0 },
+		{ "", 0 }, 
+		{ NULL }
 	};
-	char **str = strings;
 
-	while (*str) {
-		printf("check(\"%s\") -> %s\n", *str, check(*str) ?
-			"false" : "true");
-		str++;
+	for (t = tests; t->str; t++) {
+		int val = check(t->str);
+
+		printf("check(\"%s\") -> %s (%s)\n", t->str, val ?
+			"false" : "true", val == t->val ? "CORRECT": "WRONG");
 	}
 
 	return 0;
